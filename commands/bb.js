6d77689a02a9ba07dcc23adfc421bb341abd6e88
Flaip10 <facm5555@gmail.com>
2021-10-21 17:44:17 +0100

    Update rarity Files and 1/1 capability

--- commands/bb.js
+++ commands/bb.js
@@ -34,49 +34,70 @@ module.exports = {
         Token_ID = full_data[0]['Token_ID']
         token_json = getJsonData(Token_ID)
       } catch (error) {
-        message.reply(`Invalid B-Block Number.`);
+        message.reply(`Invalid B-Block Number1.`);
         return;
       }
 
       const targetChannel = message.channel
+      if(full_data[0]['Rank'] === '1'){
+        try {
+          const exampleEmbed = new MessageEmbed()
+          .setColor('#0099ff')
+          .setTitle(token_json['description'])
+          .setURL('https://opensea.io/assets/0xf8ef0631aa8150aee8cfb4db3fa27e81d78df620//' + Token_ID)
+          .addFields(
+            { name: 'Rank', value: '#'+full_data[0]['Rank'], inline: true },
+            { name: 'Token_ID', value: full_data[0]['Token_ID'], inline: false },
+          )
+          .addFields(
+            { name: token_json['attributes'][0]['trait_type'], value: token_json['attributes'][0]['value'], inline: true },
+          )
+          .setImage(token_json['image'])
+          .setTimestamp()
+          .setFooter('©Flaip', 'https://lh3.googleusercontent.com/bVGnQR2eYrE-F6wFQuE-jV0xGnN0PuAX0pE0fm58lx2ywmKwBBG68olPfYDtoAAk_sTekAU9sfhNsdRwNhTi0BgEZcyp_O9zV1GpEQ=w600');
+  
+          targetChannel.send({ embeds: [exampleEmbed] });
+        } catch (error) {
+          message.reply(`Invalid B-Block Number.`);
+        }
+      }else{
+        try {
+          const exampleEmbed = new MessageEmbed()
+          .setColor('#0099ff')
+          .setTitle(token_json['description'])
+          .setURL('https://opensea.io/assets/0xf8ef0631aa8150aee8cfb4db3fa27e81d78df620//' + Token_ID)
+          .addFields(
+            { name: 'Rank', value: '#'+full_data[0]['Rank'], inline: true },
+            { name: 'Token_ID', value: full_data[0]['Token_ID'], inline: false },
+          )
+          .addFields(
+            { name: token_json['attributes'][0]['trait_type'], value: token_json['attributes'][0]['value'], inline: true },
+            { name: token_json['attributes'][1]['trait_type'], value: token_json['attributes'][1]['value'], inline: true },
+            { name: token_json['attributes'][2]['trait_type'], value: token_json['attributes'][2]['value'], inline: true },
+          )
+          .addFields(
+            { name: token_json['attributes'][3]['trait_type'], value: token_json['attributes'][3]['value'], inline: true },
+            { name: token_json['attributes'][4]['trait_type'], value: token_json['attributes'][4]['value'], inline: true },
+            { name: token_json['attributes'][5]['trait_type'], value: token_json['attributes'][5]['value'], inline: true },
+          )
+          .addFields(
+            { name: token_json['attributes'][6]['trait_type'], value: token_json['attributes'][6]['value'], inline: true },
+            { name: token_json['attributes'][7]['trait_type'], value: token_json['attributes'][7]['value'], inline: true },
+            { name: token_json['attributes'][8]['trait_type'], value: token_json['attributes'][8]['value'], inline: true },
+          )
+          .addFields(
+            { name: token_json['attributes'][9]['trait_type'], value: token_json['attributes'][9]['value'], inline: true },
+            { name: token_json['attributes'][10]['trait_type'], value: token_json['attributes'][10]['value'], inline: true },
+            { name: token_json['attributes'][11]['trait_type'], value: token_json['attributes'][11]['value'], inline: true },
+          )
+          .setImage(token_json['image'])
+          .setTimestamp()
+          .setFooter('©Flaip', 'https://lh3.googleusercontent.com/bVGnQR2eYrE-F6wFQuE-jV0xGnN0PuAX0pE0fm58lx2ywmKwBBG68olPfYDtoAAk_sTekAU9sfhNsdRwNhTi0BgEZcyp_O9zV1GpEQ=w600');
 
-      try {
-        const exampleEmbed = new MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle(token_json['description'])
-        .setURL('https://opensea.io/assets/0xf8ef0631aa8150aee8cfb4db3fa27e81d78df620//' + Token_ID)
-        .addFields(
-          { name: 'Rank', value: '#'+full_data[0]['Rank'], inline: true },
-          { name: 'Token_ID', value: full_data[0]['Token_ID'], inline: false },
-        )
-        .addFields(
-          { name: token_json['attributes'][0]['trait_type'], value: token_json['attributes'][0]['value'], inline: true },
-          { name: token_json['attributes'][1]['trait_type'], value: token_json['attributes'][1]['value'], inline: true },
-          { name: token_json['attributes'][2]['trait_type'], value: token_json['attributes'][2]['value'], inline: true },
-        )
-        .addFields(
-          { name: token_json['attributes'][3]['trait_type'], value: token_json['attributes'][3]['value'], inline: true },
-          { name: token_json['attributes'][4]['trait_type'], value: token_json['attributes'][4]['value'], inline: true },
-          { name: token_json['attributes'][5]['trait_type'], value: token_json['attributes'][5]['value'], inline: true },
-        )
-        .addFields(
-          { name: token_json['attributes'][6]['trait_type'], value: token_json['attributes'][6]['value'], inline: true },
-          { name: token_json['attributes'][7]['trait_type'], value: token_json['attributes'][7]['value'], inline: true },
-          { name: token_json['attributes'][8]['trait_type'], value: token_json['attributes'][8]['value'], inline: true },
-        )
-        .addFields(
-          { name: token_json['attributes'][9]['trait_type'], value: token_json['attributes'][9]['value'], inline: true },
-          { name: token_json['attributes'][10]['trait_type'], value: token_json['attributes'][10]['value'], inline: true },
-          { name: token_json['attributes'][11]['trait_type'], value: token_json['attributes'][11]['value'], inline: true },
-        )
-        .setImage(token_json['image'])
-        .setTimestamp()
-        .setFooter('©Flaip', 'https://lh3.googleusercontent.com/bVGnQR2eYrE-F6wFQuE-jV0xGnN0PuAX0pE0fm58lx2ywmKwBBG68olPfYDtoAAk_sTekAU9sfhNsdRwNhTi0BgEZcyp_O9zV1GpEQ=w600');
-
-        targetChannel.send({ embeds: [exampleEmbed] });
-      } catch (error) {
-        message.reply(`Invalid B-Block Number.`);
+          targetChannel.send({ embeds: [exampleEmbed] });
+        } catch (error) {
+          message.reply(`Invalid B-Block Number.`);
+        }
       }
-
   }
 }
